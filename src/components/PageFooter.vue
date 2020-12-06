1986eceb02a39d18a80aaccb4e39179678d292bf
Jimmy javier Piamonte <jjpc56@hotmail.com>
2020-12-05 22:05:52 -0500

    Creación del componente pagefooter

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,51 @@
+<template>
+    <footer class="page-footer bg-secondary text-white pt-4 fixed-bottom">
+    <div class="container-fluid text-center text-md-left">
+      <div class="row">
+       <div class="col-md-6 mt-md-0 mt-3">
+          <h5 class="text-uppercase font-weight-bold">Motivo</h5>
+          <p>Este sitio se diseño con el fin de poner en practica los conocimientos adquiridos durante la semana 2</p>
+          <a href="https://github.com/Grupo101/semana-2-101" target="_blank">Grupo 101</a>  
+        </div>
+        <hr class="clearfix w-100 d-md-none pb-3">
+        <div class="col-md-6 mb-md-0 mb-3">
+          <h5 class="text-uppercase font-weight-bold">Contactenos</h5>
+          <div class="col">
+            <ul class="list-group bg-secondary text-white">
+                <li class="list-group-item bg-secondary text-white">
+                  <span>Nelly Alexandra Medina Rojas - nelamero@hotmail.com</span>
+                </li>
+                <li class="list-group-item bg-secondary text-white">
+                  <span>Sebastian Loaiza - sebastian.0315@hotmail.com</span>
+                </li>
+                <li class="list-group-item bg-secondary text-white">
+                  <span>Jimmy Javier Piamonte - jjpc56@hotmail.com</span>
+                </li>
+            </ul>
+          </div>
+          <div class="col">
+            <ul class="list-group">
+                <li class="list-group-item bg-secondary text-white">
+                    <span>José Daniel Delgado Ballen - jddb26@gmail.com</span>
+                </li>
+                <li class="list-group-item bg-secondary text-white">
+                    <span>Joaquin Alarcon - joaquin.alarcon@correounivalle.edu.co</span>
+                </li>
+              </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+      <a href="#"> Grupo 101</a>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+    name:"PageFooter",
+}
+</script>
+<style scoped>
+
+</style>
