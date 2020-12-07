e2627f8ab9de4f5ee0d10fdda4f308e992d18a26
nelamero <nelamero@hotmail.com>
2020-12-07 02:45:48 -0500

    ajustes de estilos

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,39 +1,39 @@
 <template>
-  <footer class="page-footer bg-secondary text-white pt-4 ">
+  <footer class="page-footer bg-secondary text-white pt-5 ">
     <div class="container-fluid text-center text-md-left">
       <div class="row">
         <div class="col-md-6 mt-md-0 mt-3">
-          <h5 class="text-uppercase font-weight-bold">Motivo</h5>
+          <h6 class="text-uppercase font-weight-bold pb-3">Motivo</h6>
           <p>
             Este sitio se diseño con el fin de poner en practica los
             conocimientos adquiridos durante la semana 2
           </p>
-          <a href="https://github.com/Grupo101/semana-2-101" target="_blank"
+          <a href="https://github.com/Grupo101/semana-2-101" class="text-decoration-none" target="_blank"
             >Grupo 101</a
           >
         </div>
         <hr class="clearfix w-100 d-md-none pb-3" />
         <div class="col-md-6 mb-md-0 mb-3">
-          <h5 class="text-uppercase font-weight-bold">Contactenos</h5>
+          <h6 class="text-uppercase font-weight-bold pb-3">Contactenos</h6>
           <div class="col">
-            <ul class="list-group bg-secondary text-white">
-              <li class="list-group-item bg-secondary text-white">
+            <ul class="list-group list-group-flush bg-secondary text-white">
+              <li class=" list-group list-group-flush bg-secondary text-white">
                 <span>Nelly Alexandra Medina Rojas - nelamero@hotmail.com</span>
               </li>
-              <li class="list-group-item bg-secondary text-white">
+              <li class="list-group list-group-flush bg-secondary text-white">
                 <span>Sebastian Loaiza - sebastian.0315@hotmail.com</span>
               </li>
-              <li class="list-group-item bg-secondary text-white">
+              <li class="list-group list-group-flush bg-secondary text-white">
                 <span>Jimmy Javier Piamonte - jjpc56@hotmail.com</span>
               </li>
             </ul>
           </div>
           <div class="col">
             <ul class="list-group">
-              <li class="list-group-item bg-secondary text-white">
+              <li class="list-group list-group-flush bg-secondary text-white">
                 <span>José Daniel Delgado Ballen - jddb26@gmail.com</span>
               </li>
-              <li class="list-group-item bg-secondary text-white">
+              <li class="list-group list-group-flush bg-secondary text-white">
                 <span
                   >Joaquin Alarcon - joaquin.alarcon@correounivalle.edu.co</span
                 >
@@ -43,9 +43,9 @@
         </div>
       </div>
     </div>
-    <div class="footer-copyright text-center py-3">
+    <div class="footer-copyright text-center py-3 pt-5">
       © 2020 Copyright:
-      <a href="#"> Grupo 101</a>
+      <a href="#" class="text-decoration-none"> Grupo 101</a>
     </div>
   </footer>
 </template>
