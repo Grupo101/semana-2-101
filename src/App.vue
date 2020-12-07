1b2f68a6b1f5f3bc607579af73febf7423cf1daa
nelamero <nelamero@hotmail.com>
2020-12-07 00:38:09 -0500

    Noticias API fin

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
   <div id="app">
+
+  
+    <div class="container-fluid">  
+      <h2  class="text-center"> NOTICIAS</h2>   
+
+
+          <section-api> </section-api>
+
+    </div>
   </div>
+
+
 </template>
 
 <script>
 
+import SectionApi from './components/SectionApi.vue'
+
 export default {
   name: 'App',
+  components:{
+    SectionApi
+    
+  }
 }
 </script>
 
